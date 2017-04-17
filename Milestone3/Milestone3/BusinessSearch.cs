2e2304a2156b7ba6dfaec789e801ab904d8c9543
Kristofer Taylor <krisrtaylor@outlook.com>
2017-04-16 23:59:28 -0700

    Hooked Up About 40% of the UI to the Database.
    All the Buttons, ComboBoxes, and DataGrids in the 'Select Location', 'Business Category', and 'Open Businsessess' groups populate/execute properly except for the 'Search Buisnessess' button.

--- Milestone3/Milestone3/BusinessSearch.cs
+++ Milestone3/Milestone3/BusinessSearch.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace Milestone3
 {
@@ -20,6 +21,138 @@ namespace Milestone3
   /// </summary>
   public partial class MainWindow : Window
   {
+        //Member Variables
+        private List<StringContainer> cityList = new List<StringContainer>();
+        private List<StringContainer> zipList = new List<StringContainer>();
+        private List<StringContainer> catList = new List<StringContainer>();
 
-  }
+
+        //Member Class Declarations
+        public class StringContainer
+        {
+            public string myString { get; set; }
+
+            public StringContainer(string newString)
+            {
+                myString = newString;
+            }
+
+            public override String ToString()
+            {
+                return myString;
+            }
+
+            public override bool Equals(System.Object obj)
+            {
+                // If parameter is null return false.
+                if (obj == null)
+                {
+                    return false;
+                }
+
+                // If parameter cannot be cast to Point return false.
+                StringContainer p = obj as StringContainer;
+                if ((System.Object)p == null)
+                {
+                    return false;
+                }
+
+                // Return true if the fields match:
+                return myString.Equals(p.ToString());
+            }
+
+            public override int GetHashCode()
+            {
+                return myString.GetHashCode();
+            }
+        }
+        
+
+        //Functions
+        //When you select an item from the 'State' dropdown box in the 'Select Location' group.
+        private void selectStateComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cityList.Clear();
+            String state = (String)selectStateComboBox.SelectedItem;
+            if(state == null) { cityDataGrid.Items.Refresh(); return; }
+
+            using (NpgsqlConnection sqlconn = new NpgsqlConnection(login))
+            {//Start SQL interaction
+                sqlconn.Open();
+                using (NpgsqlCommand cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = sqlconn;
+                    cmd.CommandText = "SELECT distinct city FROM business WHERE state = '" + state + "'order by city; ";
+
+                    using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cityList.Add(new StringContainer(reader.GetString(0)));
+                        }
+                    }
+
+                    cityDataGrid.ItemsSource = cityList;
+                    cityDataGrid.Items.Refresh();
+                }
+                sqlconn.Close();
+            }//End SQL interaction
+        }
+
+        //When you select an item from the 'City' list in the 'Select Location' group.
+        private void cityDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            zipList.Clear();
+            StringContainer cityObj = (StringContainer)cityDataGrid.SelectedItem;
+            String state = (String)selectStateComboBox.SelectedItem;
+            if (cityObj == null || state == null) { zipCodeDataGrid.Items.Refresh(); return; }
+            String city = cityObj.ToString();
+
+            using (NpgsqlConnection sqlconn = new NpgsqlConnection(login))
+            {//Start SQL interaction
+                sqlconn.Open();
+                using (NpgsqlCommand cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = sqlconn;
+                    cmd.CommandText = "SELECT distinct zipcode FROM business WHERE state = '" + state + "' AND city = '" + city + "' order by zipcode; ";
+
+                    using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                           zipList.Add(new StringContainer(reader.GetString(0)));
+                        }
+                    }
+
+                    zipCodeDataGrid.ItemsSource = zipList;
+                    zipCodeDataGrid.Items.Refresh();
+                }
+                sqlconn.Close();
+            }//End SQL interaction
+        }
+
+        //When you press the 'Add' button in the 'Business Category' group.
+        private void businessCategoryAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringContainer catObj = (StringContainer)mainBusinessCategorydataGrid.SelectedItem;
+            if (catObj == null || catList.Contains(catObj)) { return; }
+
+            catList.Add(new StringContainer(catObj.ToString()));
+
+            selectedBusinessCategorydataGrid.ItemsSource = catList;
+            selectedBusinessCategorydataGrid.Items.Refresh();
+        }
+
+        //When you press the 'Remove' button in the 'Business Category' group.
+        private void businessCategoryRemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringContainer catObj = (StringContainer)selectedBusinessCategorydataGrid.SelectedItem;
+            if (catObj == null) { return; }
+
+            catList.Remove(catObj);
+
+            selectedBusinessCategorydataGrid.ItemsSource = catList;
+            selectedBusinessCategorydataGrid.Items.Refresh();
+        }
+    }
 }
--- Milestone3/Milestone3/MainWindow.xaml.cs
+++ Milestone3/Milestone3/MainWindow.xaml.cs
@@ -12,17 +12,134 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace Milestone3
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PrimeStateDropDownBox();
+            PrimeCategoryBox();
+            //PrimeBusinessHoursDropDownBoxs();
+        }
+        
+        private void PrimeStateDropDownBox()
+        {
+            using (NpgsqlConnection sqlconn = new NpgsqlConnection(login))
+            {//Start SQL interaction
+                sqlconn.Open();
+                using (NpgsqlCommand cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = sqlconn;
+                    cmd.CommandText = "SELECT distinct state FROM business order by state; ";
+
+                    using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            selectStateComboBox.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                sqlconn.Close();
+            }//End SQL interaction
+        }
+
+        private void PrimeCategoryBox()
+        {
+            using (NpgsqlConnection sqlconn = new NpgsqlConnection(login))
+            {//Start SQL interaction
+                sqlconn.Open();
+                using (NpgsqlCommand cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = sqlconn;
+                    cmd.CommandText = "SELECT distinct name FROM category order by name; ";
+
+                    List<StringContainer> catlist = new List<StringContainer>();
+
+                    using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            catlist.Add(new StringContainer(reader.GetString(0)));
+                        }
+                    }
+
+                    mainBusinessCategorydataGrid.ItemsSource = catlist;
+                    mainBusinessCategorydataGrid.Items.Refresh();
+                }
+                sqlconn.Close();
+            }//End SQL interaction
+        }
+        /*
+        private void PrimeBusinessHoursDropDownBoxs()
+        {
+            dayOfWeekComboBox.Items.Add("Sunday");
+            dayOfWeekComboBox.Items.Add("Monday");
+            dayOfWeekComboBox.Items.Add("Tuesday");
+            dayOfWeekComboBox.Items.Add("Wednesday");
+            dayOfWeekComboBox.Items.Add("Thursday");
+            dayOfWeekComboBox.Items.Add("Friday");
+            dayOfWeekComboBox.Items.Add("Saturday");
+
+            fromComboBox.Items.Add("01:00");
+            fromComboBox.Items.Add("02:00");
+            fromComboBox.Items.Add("03:00");
+            fromComboBox.Items.Add("04:00");
+            fromComboBox.Items.Add("05:00");
+            fromComboBox.Items.Add("06:00");
+            fromComboBox.Items.Add("07:00");
+            fromComboBox.Items.Add("08:00");
+            fromComboBox.Items.Add("09:00");
+            fromComboBox.Items.Add("10:00");
+            fromComboBox.Items.Add("11:00");
+            fromComboBox.Items.Add("12:00");
+            fromComboBox.Items.Add("13:00");
+            fromComboBox.Items.Add("14:00");
+            fromComboBox.Items.Add("15:00");
+            fromComboBox.Items.Add("16:00");
+            fromComboBox.Items.Add("17:00");
+            fromComboBox.Items.Add("18:00");
+            fromComboBox.Items.Add("19:00");
+            fromComboBox.Items.Add("20:00");
+            fromComboBox.Items.Add("21:00");
+            fromComboBox.Items.Add("22:00");
+            fromComboBox.Items.Add("23:00");
+            fromComboBox.Items.Add("24:00");
+
+            toComboBox.Items.Add("01:00");
+            toComboBox.Items.Add("02:00");
+            toComboBox.Items.Add("03:00");
+            toComboBox.Items.Add("04:00");
+            toComboBox.Items.Add("05:00");
+            toComboBox.Items.Add("06:00");
+            toComboBox.Items.Add("07:00");
+            toComboBox.Items.Add("08:00");
+            toComboBox.Items.Add("09:00");
+            toComboBox.Items.Add("10:00");
+            toComboBox.Items.Add("11:00");
+            toComboBox.Items.Add("12:00");
+            toComboBox.Items.Add("13:00");
+            toComboBox.Items.Add("14:00");
+            toComboBox.Items.Add("15:00");
+            toComboBox.Items.Add("16:00");
+            toComboBox.Items.Add("17:00");
+            toComboBox.Items.Add("18:00");
+            toComboBox.Items.Add("19:00");
+            toComboBox.Items.Add("20:00");
+            toComboBox.Items.Add("21:00");
+            toComboBox.Items.Add("22:00");
+            toComboBox.Items.Add("23:00");
+            toComboBox.Items.Add("24:00");
+        }
+        */
     }
-  }
 }
